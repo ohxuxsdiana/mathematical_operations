d2e80b9a757d296acd500d57239332e174b240d8
ohxuxsdiana <baltikovadiana09@gmail.com>
2024-07-24 16:03:46 +0500

    исправила задания 1 и 2

--- task1.py
+++ task1.py
@@ -1,3 +1,6 @@
 ditels = 54
 massa_yahika = 3
-print(ditels / massa_yahika)
+a = ditels % massa_yahika
+b = ditels // massa_yahika
+c = a / a
+print(b + c)
--- task2.py
+++ task2.py
@@ -1,3 +1,5 @@
 plus = 0.1
 stalo = 8800
-print(stalo-stalo*plus)
+plus = plus + 1
+a = stalo / plus
+print(a)
